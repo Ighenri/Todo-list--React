4d9ef9b9d85d26ef00c3d6abac42142f54ff3593
Henry <igwehenry740@gmail.com>
2024-01-06 22:37:17 +0100

    todo app

---
+++ src/Todos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Todos = ({ todos }) => {
+  const todoList = todos.length ? () : ()
+
+  return <div className="todos collection">Todos</div>
+};
+
+export default Todos;
